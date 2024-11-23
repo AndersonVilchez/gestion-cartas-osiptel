0ecb4105cfd4742271f1a8acf80bf1c7cfe060ef
AndersonVilchez <andersonvilchezespinoza@gmail.com>
2024-11-22 19:04:13 -0500

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,34 @@ import streamlit as st
 import pandas as pd
 import datetime as dt
 import plotly.express as px
+import firebase_admin
+from firebase_admin import credentials, firestore
+import streamlit as st
+
+# Leer las credenciales desde los secretos de Streamlit
+firebase_config = st.secrets["firebase"]
+
+# Usar las credenciales para inicializar Firebase
+cred = credentials.Certificate({
+    "type": firebase_config["type"],
+    "project_id": firebase_config["project_id"],
+    "private_key_id": firebase_config["private_key_id"],
+    "private_key": firebase_config["private_key"],
+    "client_email": firebase_config["client_email"],
+    "client_id": firebase_config["client_id"],
+    "auth_uri": firebase_config["auth_uri"],
+    "token_uri": firebase_config["token_uri"],
+    "auth_provider_x509_cert_url": firebase_config["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": firebase_config["client_x509_cert_url"]
+})
+
+# Inicializa la app Firebase
+firebase_admin.initialize_app(cred)
+
+# Conectar a Firestore
+db = firestore.client()
+
+# Ahora puedes interactuar con Firestore
 
 # Inicialización de la base de datos en la sesión
 if "cartas_db" not in st.session_state:
